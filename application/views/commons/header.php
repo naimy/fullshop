a127f5333c50370ec009f1fe5eb08d5b2732464e
Naimy <jonat.palumbo@gmail.com>
2017-07-01 10:59:02 +0200

    First commit

---
+++ application/views/commons/header.php
@@ -0,0 +1,67 @@
+<?php var_dump(BASEPATH) ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <video poster="https://www.youtube.com/watch?v=oT8qOG9iAHA" id="bgvid" playsinline autoplay muted loop>
+                <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+                <source src="https://www.youtube.com/watch?v=oT8qOG9iAHA" type="video/webm">
+                <source src="<?php echo $application_folder ?>/assets/video/bg.mp4" type="video/mp4">
+            </video>
+            <header>
+
+            </header>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">EffectImmo</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="navbar-form navbar-left">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+    </div>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="row">
+    <div class="col-12">
+        <h1>Titre</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ornare orci vestibulum nibh varius, ullamcorper feugiat lectus consectetur. Nulla erat metus, pulvinar sed hendrerit eget, laoreet quis metus. Maecenas condimentum diam dui, vitae viverra nisi sagittis non. In congue orci eu libero tristique blandit. Nunc nec leo orci. Proin mollis arcu nulla, sed maximus velit suscipit eu. Nunc molestie luctus nunc, sit amet imperdiet enim finibus id. Nullam id metus at felis commodo egestas. Etiam est nulla, convallis et mauris sed, semper suscipit lorem.
+
+            Sed blandit, metus sit amet tristique sodales, nulla eros interdum nunc, non fermentum lectus arcu et sapien. Morbi convallis aliquam malesuada. Mauris et ipsum enim. Sed sit amet rhoncus diam, eu pharetra tellus. Suspendisse lobortis cursus pretium. Nulla facilisi. Duis vitae malesuada ligula. Nullam venenatis odio vitae nibh vehicula, eget ornare neque facilisis. Etiam felis lorem, auctor in mollis in, vulputate et odio. Nam pretium et velit nec dignissim. Etiam imperdiet maximus purus, efficitur vulputate lorem porta sit amet. Etiam eget placerat tortor. Aenean accumsan, odio non finibus viverra, nunc sem aliquam turpis, vel efficitur tortor ligula nec erat.
+
+            Curabitur et molestie dolor, quis consequat mi. Sed mattis orci ac consequat efficitur. Phasellus rhoncus ante id urna viverra, et vehicula nunc finibus. Etiam id ipsum quis odio posuere maximus. Vestibulum molestie vitae velit ac convallis. Mauris nulla ex, volutpat ac efficitur non, rutrum sed magna. Aenean arcu leo, tincidunt vel sapien eget, congue tincidunt erat.
+
+            Nullam malesuada, nunc non iaculis feugiat, nunc dui lacinia ante, sit amet dictum nisl odio at velit. Nulla accumsan ultrices porta. Nulla ac dignissim erat. Maecenas sodales tincidunt efficitur. Quisque non tortor vehicula, semper mi semper, tincidunt leo. Vivamus consectetur tempor nisl a molestie. Aenean porttitor vehicula diam vitae pellentesque. Donec sed euismod lorem, vitae elementum arcu. Duis vestibulum lacinia nisi in euismod. Integer gravida metus in leo dictum viverra. Pellentesque ac nunc imperdiet, tristique velit eget, pharetra leo. Nam nec facilisis ex. Sed quis purus nunc.
+
+            Sed ornare, tortor sed ultricies vulputate, enim libero cursus purus, in dignissim leo eros non nisl. Mauris congue mauris nec risus malesuada luctus. Nam felis diam, sollicitudin et interdum ut, maximus in orci. Maecenas egestas, felis in pretium hendrerit, nisi mi consequat nisi, vitae sodales nibh tortor sagittis arcu. Praesent vitae orci est. Sed enim nisi, posuere eu elit sit amet, tempus sodales neque. Integer rutrum consectetur aliquet. Nam pretium, leo nec mattis ullamcorper, ex libero sollicitudin lorem, nec consectetur massa nibh id ante. Etiam pretium, enim vitae tempus ornare, lacus leo ultrices neque, vel porta purus lorem eget nunc. Quisque quis tellus pulvinar, sollicitudin ante quis, imperdiet neque. Vestibulum ac sollicitudin risus, at cursus libero. Nam a lacus consequat, lobortis enim non, faucibus nulla. Etiam pellentesque molestie ipsum ac viverra. Quisque euismod leo sed consectetur imperdiet. Vivamus vitae fermentum nulla. Praesent nec ante at orci sagittis porttitor.
+        </p>
+        <button>Estimer votre bien</button>
+    </div>
+</div>
+
